067a603ceaae4f3d6024ff412c43b18f698d5452
WendigoTV <54577070+WendigoTV@users.noreply.github.com>
2020-04-25 18:33:39 +0200

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace MixGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.glass1 = new System.Windows.Forms.PictureBox();
+            this.glass2 = new System.Windows.Forms.PictureBox();
+            this.glass3 = new System.Windows.Forms.PictureBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.marble = new System.Windows.Forms.PictureBox();
+            this.animationFPS = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.glass1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.glass2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.glass3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.marble)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // glass1
+            // 
+            this.glass1.Image = global::MixGame.Properties.Resources.GlassEmpty;
+            this.glass1.Location = new System.Drawing.Point(11, 65);
+            this.glass1.Margin = new System.Windows.Forms.Padding(2);
+            this.glass1.Name = "glass1";
+            this.glass1.Size = new System.Drawing.Size(148, 205);
+            this.glass1.TabIndex = 0;
+            this.glass1.TabStop = false;
+            // 
+            // glass2
+            // 
+            this.glass2.Image = global::MixGame.Properties.Resources.GlassEmpty;
+            this.glass2.Location = new System.Drawing.Point(227, 65);
+            this.glass2.Margin = new System.Windows.Forms.Padding(2);
+            this.glass2.Name = "glass2";
+            this.glass2.Size = new System.Drawing.Size(148, 205);
+            this.glass2.TabIndex = 1;
+            this.glass2.TabStop = false;
+            // 
+            // glass3
+            // 
+            this.glass3.Image = global::MixGame.Properties.Resources.GlassEmpty;
+            this.glass3.Location = new System.Drawing.Point(441, 65);
+            this.glass3.Margin = new System.Windows.Forms.Padding(2);
+            this.glass3.Name = "glass3";
+            this.glass3.Size = new System.Drawing.Size(148, 205);
+            this.glass3.TabIndex = 2;
+            this.glass3.TabStop = false;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(227, 284);
+            this.startButton.Margin = new System.Windows.Forms.Padding(2);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(148, 62);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // marble
+            // 
+            this.marble.Image = global::MixGame.Properties.Resources.Marble;
+            this.marble.Location = new System.Drawing.Point(264, 145);
+            this.marble.Margin = new System.Windows.Forms.Padding(2);
+            this.marble.Name = "marble";
+            this.marble.Size = new System.Drawing.Size(72, 72);
+            this.marble.TabIndex = 4;
+            this.marble.TabStop = false;
+            // 
+            // animationFPS
+            // 
+            this.animationFPS.Tick += new System.EventHandler(this.animationFPS_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.marble);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.glass3);
+            this.Controls.Add(this.glass2);
+            this.Controls.Add(this.glass1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.glass1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.glass2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.glass3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.marble)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox glass1;
+        private System.Windows.Forms.PictureBox glass2;
+        private System.Windows.Forms.PictureBox glass3;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox marble;
+        private System.Windows.Forms.Timer animationFPS;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MixGame
+{
+    public partial class Form1 : Form
+    {
+        bool isMoving = false;
+        public static readonly Point marble1DefaultLocation = new Point(45, 145);
+        public static readonly Point marble2DefaultLocation = new Point(263, 145);
+        public static readonly Point marble3DefaultLocation = new Point(478, 145);
+        public static readonly Point glass1DefaultLocation = new Point(11, 65);
+        public static readonly Point glass2DefaultLocation = new Point(227, 65);
+        public static readonly Point glass3DefaultLocation = new Point(441, 65);
+        Point pastLocation1;
+        Point pastLocation2;
+        public List<PictureBox> glassList;
+        public List<Point> glassDefaultLocationList;
+        public List<Point> marbleDefaultLocationList;
+        Random rnd = new Random();
+        int marbleLoc;
+        int firstToSwap;
+        int secondToSwap;
+        int speed = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            resetGlassPosition(false);
+            glassList = new List<PictureBox>() { glass1, glass2, glass3 };
+            glassDefaultLocationList = new List<Point>() { glass1DefaultLocation, glass2DefaultLocation, glass3DefaultLocation };
+            marbleDefaultLocationList = new List<Point>() { marble1DefaultLocation, marble2DefaultLocation, marble3DefaultLocation };
+            marble.Visible = true;
+            marble.Location = marble2DefaultLocation;
+            animationFPS.Interval = 1;
+            animationFPS.Start();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (speed < 5 || speed > 25)
+            {
+                return;
+
+            } else { 
+
+            if (!isMoving)
+            {
+                marble.Visible = false;
+                marble.BringToFront();
+                whichToSwap();
+                startMixing();
+                }
+            }
+        }
+
+
+        private void startMixing()
+        {
+
+            marbleLoc = rnd.Next(0, 3);
+
+            switch (marbleLoc)
+            {
+                case 1:
+                    marble.Location = marble1DefaultLocation;
+                    break;
+                case 2:
+                    marble.Location = marble2DefaultLocation;
+                    break;
+                case 3:
+                    marble.Location = marble3DefaultLocation;
+                    break;
+            }
+
+            whichToSwap();
+
+            pastLocation1 = glassList[firstToSwap].Location;
+            pastLocation2 = glassList[secondToSwap].Location;
+            
+            isMoving = true;
+
+
+        }
+
+        private void resetGlassPosition(bool afterSwap)
+        {
+            if (afterSwap == false)
+            {
+                glass1.Location = glass1DefaultLocation;
+                glass2.Location = glass2DefaultLocation;
+                glass3.Location = glass3DefaultLocation;
+
+            } else {
+                glassList[firstToSwap].Location = glassDefaultLocationList[firstToSwap];
+                glassList[secondToSwap].Location = glassDefaultLocationList[secondToSwap];
+            }
+        }
+
+        private void animationFPS_Tick(object sender, EventArgs e)
+        {
+            if (isMoving)
+            {
+                for (int a = 0; a < speed; a++) {
+
+                    glassList[firstToSwap].Location = new Point(glassList[firstToSwap].Location.X + 1, glassList[firstToSwap].Location.Y);
+                    glassList[secondToSwap].Location = new Point(glassList[secondToSwap].Location.X - 1, glassList[secondToSwap].Location.Y);
+
+                    if (glassList[firstToSwap].Location == pastLocation2 || glassList[secondToSwap].Location == pastLocation1)
+                    {
+                        swapIndexInList();
+                        isMoving = false;
+                    }
+                }
+            } else {
+                resetGlassPosition(true);
+            }
+        }
+
+        private void whichToSwap()
+        {
+            do
+            {
+                firstToSwap = rnd.Next(0, 2);
+                secondToSwap = rnd.Next(1, 3);
+
+            } while (firstToSwap == secondToSwap);
+        }
+
+        private void swapIndexInList()
+        {
+            PictureBox tmp = glassList[firstToSwap];
+            glassList[firstToSwap] = glassList[secondToSwap];
+            glassList[secondToSwap] = tmp;
+        }
+    }
+}
